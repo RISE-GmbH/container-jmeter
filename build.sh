5bed7c0d391cf50a15732ea1fe3b37661f5fa48e
Markus Mayr <markus.mayr@rise-world.com>
2023-11-01 19:08:40 +0100

    auto save built image

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"Europe/Amsterdam"}
 # Example build line
 podman build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "rise/jmeter:${JMETER_VERSION}" .
 podman tag "rise/jmeter:${JMETER_VERSION}" "rise/jmeter:latest"
+
+podman image save --format docker-archive rise/jmeter:${JMETER_VERSION} | gzip > rise_jmeter_${JMETER_VERSION}.tar.gz
